c38bf37a23c66435bec46e47ce891eb22aad3e00
Stefano Della Morte <stefano.dellamorte@cybertec.it>
2023-03-30 14:45:13 +0200

    Decoupled bot from handlers

--- src/bot.ts
+++ src/bot.ts
@@ -1,8 +1,4 @@
 import TelegramBot from 'node-telegram-bot-api';
-import CommandHandler from './handlers/CommandHandler';
-import InlineQueryHandler from './handlers/InlineQueryHandler';
-import PollAnswerHandler from './handlers/PollAnswerHandler';
-import TextHandler from './handlers/TextHandler';
 
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token as string);
@@ -12,17 +8,5 @@ global = {
     ...global,
     bot: bot,
 }
-
-// Commands
-bot.onText(/^\/\w+$/, CommandHandler.handlerFunction);
-
-// Callback queries
-bot.on('callback_query', InlineQueryHandler.handlerFunction);
-
-// Text answers
-bot.onText(/.+/, TextHandler.handlerFunction);
-
-// Poll answers
-bot.on('poll_answer', PollAnswerHandler.handlerFunction);
 
 export default bot;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import bot from './bot';
 import { Controller } from './controller';
+import CommandHandler from './handlers/CommandHandler';
+import InlineQueryHandler from './handlers/InlineQueryHandler';
+import PollAnswerHandler from './handlers/PollAnswerHandler';
+import TextHandler from './handlers/TextHandler';
 
 async function main() {
     await Controller.connect();
     console.log("Start polling...");
+
+    // development handlers
+    bot.onText(/^\/\w+$/, CommandHandler.handlerFunction);
+    bot.on('callback_query', InlineQueryHandler.handlerFunction);
+    bot.onText(/.+/, TextHandler.handlerFunction);
+    bot.on('poll_answer', PollAnswerHandler.handlerFunction);
+
     await bot.startPolling();
 }
 
