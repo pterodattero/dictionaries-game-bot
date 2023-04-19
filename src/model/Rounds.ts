8ad3b33d26c6a5c4d1a1db0ca946400fb696a168
Stefano Della Morte <stefano.dellamorte@cybertec.it>
2023-04-19 10:49:13 +0200

    Added game identifier to round history

--- src/model/Rounds.ts
+++ src/model/Rounds.ts
@@ -2,6 +2,7 @@ import { Schema, model } from "mongoose";
 
 interface IRound {
     chatId: number,
+    startMessageId: number,
     pollMessageId: number,
     votes: {
         userId: number,
@@ -11,12 +12,13 @@ interface IRound {
 
 const RoundSchema = new Schema<IRound>({
     chatId: { type: Number, required: true },
+    startMessageId: { type: Number, required: true },
     pollMessageId: { type: Number, required: true },
     votes: [{
         userId: Number,
         votes: [Number]
     }],
 })
-RoundSchema.index({ chatId: 1, pollMessageId: 1 }, { unique: true });
+RoundSchema.index({ chatId: 1, startMessageId: 1, pollMessageId: 1 }, { unique: true });
 
 export default model('Round', RoundSchema);
